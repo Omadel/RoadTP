cdd99318799c221bb0165b5db5cfb1e11f01bd0a
HonorineF <65893967+HonorineF@users.noreply.github.com>
2021-12-09 17:30:55 +0100

    up

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,28 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Camera cam;
+    private float _screenDistance = 70;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        if(Input.GetKey(KeyCode.W) || Input.mousePosition.y > Screen.height - _screenDistance)
         {
             transform.position += Vector3.forward * speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D) || Input.mousePosition.x > Screen.width - _screenDistance)
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S) || Input.mousePosition.y < _screenDistance)
         {
             transform.position += Vector3.back * speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A) || Input.mousePosition.x < _screenDistance)
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
         }
@@ -27,6 +33,5 @@ public class CameraController : MonoBehaviour
         {
             cam.transform.localPosition += cam.transform.forward * Input.mouseScrollDelta.y;
         }
-
     }
 }
